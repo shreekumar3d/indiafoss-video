6e9505aa209b121c70c61b2fa46f167ccfea8ccf
Shree Kumar <shree.shree@gmail.com>
2025-09-17 00:11:06 +0530

    Handle speaker names, designations, company cases
    If 2 speakers then we only show company names.
    1 speaker then we show designation and two rows for company name.
    Seems to be good for current speaker selections.

--- obs/gen-session-scene-images.py
+++ obs/gen-session-scene-images.py
@@ -22,14 +22,14 @@ def get_text_width_mm(text, family='Inter', size=28, dpi=78):
     width_mm = width * 25.4 / dpi
     return width_mm
 
-def span_text(text):
+def span_text(text, size, limit):
     words = text.split(' ')
     lines = []
     this_line = []
     for word in words:
         test_line = ' '.join(this_line+[word])
-        tw = get_text_width_mm(test_line)
-        if tw > 460: # limit in template
+        tw = get_text_width_mm(test_line, size=size)
+        if tw > limit:
             partial_line = ' '.join(this_line)
             lines.append(partial_line)
             this_line = []
@@ -45,13 +45,22 @@ def gen_speaker_plus_slides(talk_info):
 
     title = talk_info['title'].strip()
 
-    title_line = span_text(title)
+    title_line = span_text(title, 28, 460) # limit from template
     if len(title_line)>2:
         raise ValueError("Too long title:", title)
 
+    title_only = ""
+    title_line1 = ""
+    title_line2 = ""
+
     track = talk_info['track']
-    title_line1 = title_line[0]
-    title_line2 = title_line[1] if len(title_line)>1 else ""
+    if len(title_line)==1:
+        title_only = title_line[0]
+        title_line1 = ""
+        title_line2 = ""
+    else:
+        title_line1 = title_line[0]
+        title_line2 = title_line[1]
 
     speaker_info = talk_info['speakers'][0]
     speaker1 = speaker_info['name']
@@ -64,15 +73,32 @@ def gen_speaker_plus_slides(talk_info):
     if len(talk_info['speakers'])>1:
         speaker_info = talk_info['speakers'][1]
         speaker2 = speaker_info['name']
-        speaker2_designation = speaker_info['designation']
-        speaker2_affiliation = speaker_info['company']
+        if len(speaker_info['company'])>0:
+            speaker2_designation = speaker_info['company']
+        else:
+            speaker2_designation = speaker_info['designation']
+        if len(speaker1_company)>0:
+            speaker1_designation = speaker1_company
+            speaker1_company = ""
 
+    company_line = span_text(speaker1_company, 24, 120) # limit from template
+    if len(company_line)>2:
+        raise ValueError("Company name too long:", speaker1_company)
+    else:
+        speaker1_company1 = company_line[0]
+        speaker1_company2 = ""
+        if len(company_line)>1:
+            speaker1_company2 = company_line[1]
+        #print(speaker1_company1)
+        #print(speaker1_company2)
     template = open("templates/talk-presentation-section.svg","r").read()
     template = template.replace('$TITLE-LINE1$', escape(title_line1))
     template = template.replace('$TITLE-LINE2$', escape(title_line2))
+    template = template.replace('$TITLE-ONLY$', escape(title_only))
     template = template.replace('$SPEAKER1$', escape(speaker1))
     template = template.replace('$SPEAKER1-DESIGNATION$', escape(speaker1_designation))
-    template = template.replace('$SPEAKER1-COMPANY$', escape(speaker1_company))
+    template = template.replace('$SPEAKER1-COMPANY1$', escape(speaker1_company1))
+    template = template.replace('$SPEAKER1-COMPANY2$', escape(speaker1_company2))
     template = template.replace('$SPEAKER2$', escape(speaker2))
     template = template.replace('$SPEAKER2-DESIGNATION$', escape(speaker2_designation))
     template = template.replace('$TEMPLATE-IMAGE-DIR$', template_image_dir, -1)
@@ -83,11 +109,15 @@ def gen_speaker_plus_slides(talk_info):
 
     track_dir = Path(track)
     track_dir.mkdir(parents=True, exist_ok=True)
-    output_file = os.path.join(track_dir,'_'.join(title_line1.split(' ')[:5])+'.png')
+    #output_file = os.path.join(track_dir,'_'.join(title.split(' ')[:4]))+'.png'
+    output_file = ('_'.join(title.split(' ')[:4]))+'.png'
+    output_file = output_file.replace(':','',-1) # remove troublesome chars in filename
     cmd = ['inkscape', '--export-type=png', '--export-width=1920',
            '--export-height=1080', '--export-filename', output_file, 'out.svg']
     print(output_file)
     subprocess.run(cmd, check=True)
+    # ensure the file actually got created
+    open(output_file,'r')
 
 # Load all talk metadata
 talks = []
